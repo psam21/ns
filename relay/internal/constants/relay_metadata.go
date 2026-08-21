a0c6ddcc17b1963cc43088cfe2dd4f663094596f
psam21 <psam1981@gmail.com>
2026-08-21 17:21:06 +0530

    ref #30: register NIP-19 bech32-encoded entities in supported NIPs

--- relay/internal/constants/relay_metadata.go
+++ relay/internal/constants/relay_metadata.go
@@ -34,6 +34,7 @@ var DefaultSupportedNIPs = []interface{}{
 	13, // NIP-13: Proof of Work
 	17, // NIP-17: Private Direct Messages
 	18, // NIP-18: Reposts
+	19, // NIP-19: bech32-encoded entities (npub, nsec, note, nprofile, nevent, naddr)
 	21, // NIP-21: nostr: URI scheme
 	22, // NIP-22: Comment
 	23, // NIP-23: Long-form Content
